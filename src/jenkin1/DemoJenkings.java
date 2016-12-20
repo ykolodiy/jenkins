adf07f65edbf0a1b400172f52da0fe1177d12851
vkoloyu <yuriw@ukr.net>
2016-12-20 13:06:04 -0600

    first

---
+++ src/jenkin1/DemoJenkings.java
@@ -0,0 +1,27 @@
+package jenkin1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class DemoJenkings {
+
+	@Test
+	public void testJ(){
+		
+		System.out.println("done whoooooooooooo");
+		
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+			
+			driver.manage().window().maximize();
+
+        // And now use this to visit Google
+        driver.get("http://www.google.com");
+        System.out.println(driver.getTitle());
+        driver.quit();
+		
+		
+	}
+}
